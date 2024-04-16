f40f6300c09765c40e68f6cb29fba1f49193a0ef
Ardit Cali <129833461+arditcali@users.noreply.github.com>
2024-04-16 12:10:28 +0100

    Add files via upload

---
+++ Guessing Game/Guessing Game/Program.cs
@@ -0,0 +1,47 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        bool playAgain = true;
+        int min = 1;
+        int max = 100;
+        int guess = 0;
+        int number = 0;
+        int guesses = 0;
+        String answer = "";
+
+        while(playAgain) {
+            number = random.Next(min, max + 1);
+
+            while(number != guess) {
+                Console.WriteLine("Guess a number betwen 1 - 100");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess > number) {
+                    Console.WriteLine("Number too high");    
+                }
+                else if(guess < number) {
+                    Console.WriteLine("Number too low");
+                }
+                guesses++;
+            }
+            Console.WriteLine("Number is " + number);
+            Console.WriteLine("You WIN!");
+            Console.WriteLine("Guesses: " + guesses);
+
+            Console.WriteLine();
+            Console.WriteLine("Do you want to play again?");
+            Console.WriteLine("Press: Y for (yes) and N for (no)");
+            answer = Console.ReadLine();
+
+            if(answer.Equals("y")) {
+                playAgain = true;
+            }else {
+                playAgain = false;
+            }
+        }
+
+        
+    }
+}
