5cc4122453bbbc61d05f0df43273209fe0ce5bae
Ardit Cali <129833461+arditcali@users.noreply.github.com>
2024-04-17 16:09:23 +0100

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Formats.Asn1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        do {
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+            char option;
+
+            Console.WriteLine("CALCULATOR");
+
+            Console.Write("Enter first number: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter option: ");
+            option = Convert.ToChar(Console.ReadLine());
+
+            switch(option) {
+            case '+': Console.WriteLine("Result: " + (num1 + num2));
+                break;
+            case '-': Console.WriteLine("Result: " + (num1 - num2));
+                break;
+            case '*': Console.WriteLine("Result: " + num1 * num2);
+                break;
+            case '/': Console.WriteLine("Result: " + num1 / num2);
+                break;          
+            }
+            Console.WriteLine("Continue?");
+            Console.WriteLine("yes - no");
+
+        }while(Console.ReadLine() == "yes");   
+    }
+}
